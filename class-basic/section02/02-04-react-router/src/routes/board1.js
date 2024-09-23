98a4dcea25a54b74f7e12a5b28b9f70d1dd74102
yheseul <kangyheseul@naver.com>
2024-09-23 12:52:12 +0900

    .

--- class-basic/section02/02-04-react-router/src/routes/board1.js
+++ class-basic/section02/02-04-react-router/src/routes/board1.js
@@ -4,17 +4,21 @@ const Board1 = () => {
   const [writer, setIWriter] = useState("");
   const [title, setITitle] = useState("");
   const [content, setIContent] = useState("");
+  const [isActive, setIsActive] = useState(false);
 
   const onChangeWriter = (event) => {
     setIWriter(event.target.value);
+    event.target.value && title && content ? setIsActive(true) : setIsActive(false);
   };
 
   const onChangeTitle = (event) => {
     setITitle(event.target.value);
+    writer && event.target.value && content ? setIsActive(true) : setIsActive(false);
   };
 
   const onChangeContent = (event) => {
     setIContent(event.target.value);
+    writer && title && event.target.value ? setIsActive(true) : setIsActive(false);
   };
 
   const onClickSubmit = () => {
@@ -26,10 +30,18 @@ const Board1 = () => {
 
   return (
     <>
-      writer : <input type="text" onChange={onChangeWriter} /><br />
-      title : <input type="text" onChange={onChangeTitle} /><br />
-      content : <input type="text" onChange={onChangeContent} /><br />
-      <button onClick={onClickSubmit}>submit</button>
+      writer : <input type="text" onChange={onChangeWriter} />
+      <br />
+      title : <input type="text" onChange={onChangeTitle} />
+      <br />
+      content : <input type="text" onChange={onChangeContent} />
+      <br />
+      <button
+        onClick={onClickSubmit}
+        style={{ backgroundColor: isActive ? "yellow" : "gray" }}
+      >
+        submit
+      </button>
     </>
   );
 };
