fb145f0d3c8377dd9c0d5b71d5338d15a3a56924
yheseul <kangyheseul@naver.com>
2024-10-11 12:30:45 +0900

    HIDDEN_HEADERS

--- class-example/my-app/src/commons/layout/index.tsx
+++ class-example/my-app/src/commons/layout/index.tsx
@@ -1,13 +1,26 @@
+"use client";
+
 import { ReactNode } from "react";
 import LayoutNavigation from "./navigation";
 import LayoutHeader from "./header";
 import LayoutBanner from "./banner";
 import LayoutFooter from "./footer";
+import { usePathname } from "next/navigation";
+
+const HIDDEN_HEADERS = [
+  "/section12/12-03-library-carousel",
+  "/section12/12-05-modal-alert",
+];
 
 export default function Layout({ children }: { children: ReactNode }) {
+  const pathname = usePathname();
+  console.log(pathname);
+
+  const isHiddenHeader = HIDDEN_HEADERS.includes(pathname);
+
   return (
     <div>
-      <LayoutHeader />
+      {!isHiddenHeader && <LayoutHeader />}
       <LayoutBanner />
       <LayoutNavigation />
       <div style={{ display: "flex" }}>
