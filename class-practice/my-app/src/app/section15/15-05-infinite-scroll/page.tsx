4f5ef5d1c73ca794e0ab68472be4137e52ff8778
yheseul <kangyheseul@naver.com>
2024-10-15 13:05:21 +0900

    15-05-infinite-scroll

---
+++ class-practice/my-app/src/app/section15/15-05-infinite-scroll/page.tsx
@@ -0,0 +1,58 @@
+"use client";
+
+import { DingtalkSquareFilled } from "@ant-design/icons";
+import { gql, useQuery } from "@apollo/client";
+import { useState } from "react";
+import InfiniteScroll from "react-infinite-scroll-component";
+
+const FETCH_BOARDS = gql`
+  query fetchBoards($page: Int) {
+    fetchBoards(page: $page) {
+      _id
+      writer
+      title
+      contents
+    }
+  }
+`;
+
+export default function InfiniteScrollPage() {
+  const [hasMore, setHasMore] = useState(true);
+  const { data, fetchMore } = useQuery(FETCH_BOARDS);
+
+  const onNext = () => {
+    if (data === undefined) return;
+
+    fetchMore({
+      variables: { page: Math.ceil(data?.fetchBoards.length / 10) + 1 },
+      updateQuery: (prev, { fetchMoreResult }) => {
+        if (!fetchMoreResult.fetchBoards?.length) {
+          setHasMore(false);
+          return;
+        }
+
+        return {
+          fetchBoards: [...prev.fetchBoards, ...fetchMoreResult.fetchBoards],
+        };
+      },
+    });
+  };
+
+  return (
+    <div>
+      <InfiniteScroll
+        next={onNext}
+        hasMore={hasMore}
+        loader={<div>loading...</div>}
+        dataLength={data?.fetchBoards.length ?? 0}
+      >
+        {data?.fetchBoards.map((el) => (
+          <div key={el.number}>
+            <span style={{ margin: "10px" }}>{el.title}</span>
+            <span style={{ margin: "10px" }}>{el.writer}</span>
+          </div>
+        ))}
+      </InfiniteScroll>
+    </div>
+  );
+}
