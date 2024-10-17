f09e3d565817fdb055f5189f1ea09083abda6575
yheseul <kangyheseul@naver.com>
2024-10-17 12:24:39 +0900

    17-03-decorator

---
+++ supplement/section17/17-03-decorator/decorator-example.ts
@@ -0,0 +1,12 @@
+function bbb(a: any) {
+  console.log("============");
+  console.log(a);
+  console.log("============");
+}
+
+@bbb
+class User {
+  // id: number
+  // firstName: string
+  // ...
+}
