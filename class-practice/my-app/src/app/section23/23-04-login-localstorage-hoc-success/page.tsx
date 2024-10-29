84dfb024431bb87b7c075364561d6078dfb5c0c7
yheseul <kangyheseul@naver.com>
2024-10-29 13:19:26 +0900

    23-04-login-localstorage-hoc-check

---
+++ class-practice/my-app/src/app/section23/23-04-login-localstorage-hoc-success/page.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import { withLoginCheck } from "@/commons/hocs/23-04-withLoginCheck";
+import { gql, useQuery } from "@apollo/client";
+
+const FETCH_USER_LOGGED_IN = gql`
+  query fetchUserLoggedIn {
+    fetchUserLoggedIn {
+      _id
+      email
+      name
+    }
+  }
+`;
+
+function LoginSuccess() {
+  const { data } = useQuery(FETCH_USER_LOGGED_IN);
+
+  return <div>WellCome. {data?.fetchUserLoggedIn.name}</div>;
+}
+
+export default withLoginCheck(LoginSuccess);
