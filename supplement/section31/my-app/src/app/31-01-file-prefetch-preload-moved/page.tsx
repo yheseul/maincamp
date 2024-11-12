b715e9954f3448e60837a888aa38b6d92f13a48e
yheseul <kangyheseul@naver.com>
2024-11-12 12:57:54 +0900

    31-01-file-prefetch-preload

---
+++ supplement/section31/my-app/src/app/31-01-file-prefetch-preload-moved/page.tsx
@@ -0,0 +1,3 @@
+export default function FilePrefetchPreloadMoved() {
+  return <div>hi</div>;
+}
---
+++ supplement/section31/my-app/src/app/31-01-file-prefetch-preload/page.tsx
@@ -0,0 +1,5 @@
+import Link from "next/link";
+
+export default function FilePrefetchPreload() {
+  return <Link href={"/31-01-file-prefetch-preload-moved"}>move</Link>;
+}
