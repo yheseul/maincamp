f6fca91f29ef3a1614eaf1c825daa986ac145dfc
yheseul <kangyheseul@naver.com>
2024-11-13 17:07:51 +0900

    32-01-html-scraping

---
+++ class-practice/my-app/src/app/section32/32-01-html-scraping/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+export default function HtmlScrapingPage() {
+  const onClickScraping = async () => {
+    const result = await fetch("http://localhost:3000/section19/19-01-search");
+    const data = await result.text();
+    console.log(data);
+  };
+  return <button onClick={onClickScraping}>Scraping</button>;
+}
