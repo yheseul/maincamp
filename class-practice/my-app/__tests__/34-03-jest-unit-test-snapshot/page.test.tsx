ccd8af67261e912c5240e1b625a62d7078814d52
yheseul <kangyheseul@naver.com>
2024-11-20 20:27:55 +0900

    34-03-jest-unit-test-snapshot

---
+++ class-practice/my-app/__tests__/34-03-jest-unit-test-snapshot/page.test.tsx
@@ -0,0 +1,8 @@
+import { render } from "@testing-library/react";
+import "@testing-library/dom";
+import JestUnitTestSnapshot from "@/app/section34/34-03-jest-unit-test-snapshot/page";
+
+it("기존 사진이랑 바뀐 거 비교 - snapshot test", () => {
+  const result = render(<JestUnitTestSnapshot />);
+  expect(result.container).toMatchSnapshot();
+});
---
+++ class-practice/my-app/src/app/section34/34-03-jest-unit-test-snapshot/page.tsx
@@ -0,0 +1,9 @@
+export default function JestUnitTestSnapshot() {
+  return (
+    <div>
+      <div>hello</div>
+      hobby: <input type="text" />
+      <button>go</button>
+    </div>
+  );
+}
